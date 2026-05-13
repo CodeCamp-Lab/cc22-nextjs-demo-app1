29a14f2ae30715ead9be288ceaef626087a8d846
Prince <princesuke@gmail.com>
2026-05-13 10:23:52 +0700

    feat: add create-post

---
+++ app/create-post/page.tsx
@@ -0,0 +1,22 @@
+export default function CreatePostPage() {
+  return (
+    <div className="px-6 py-12">
+      <h1 className="text-2xl font-bold mb-8">Create Post</h1>
+      <form className="space-y-3">
+        <div>
+          <p>Title:</p>
+          <input name="title" className="border p-2 rounded" />
+        </div>
+
+        <div>
+          <p>Content</p>
+          <textarea name="content" className="border p-2 rounded" />
+        </div>
+
+        <button className="border rounded p-2 cursor-pointer bg-blue-600 text-white">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
